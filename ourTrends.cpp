8a8b1b0706f95e7b61836713035bfc1cafe8ace0
meslerke <meslerke@miamioh.edu>
2014-11-24 09:48:53 -0500

    Created a header file that extends NaiveTrends.h and attempted to create an instance of an unordered map. Copied the code from the naiveTrends.cpp file to this file in hopes of editing the methods.

---
+++ ourTrends.cpp
@@ -0,0 +1,55 @@
+#include "naiveTrends.h"
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+
+//hopefully just created an object of type unordered_map, not sure if it is correct
+typedef std::unordered_map<std::string, std::string> ourMap;
+
+void naiveTrends::increaseCount(std::string s, unsigned int amount){
+	//Check to see if word is already present
+	for (unsigned int i = 0; i < wordCountVector.size(); i++){
+		if (wordCountVector[i].first == s){
+			//If so, just increment the "amount"
+			wordCountVector[i].second += amount;
+			return;
+		}
+	}
+	//If not, add a new item to the list.
+	wordCountVector.push_back(std::make_pair(s, amount));
+}
+
+unsigned int naiveTrends::getCount(std::string s){
+	//Check to see if word is present
+	for (unsigned int i = 0; i < wordCountVector.size(); i++){
+		if (wordCountVector[i].first == s){
+			//If so, return the count
+			return wordCountVector[i].second;
+		}
+	}
+	//otherwise, return 0
+	return 0;
+}
+
+//Keeping Brinkman's code for this method
+bool compareFunc(std::pair<std::string, unsigned int> i, std::pair<std::string, unsigned int> j) {
+	if (i.second == j.second){
+		return (i.first < j.first);
+	}
+
+	return (i.second > j.second);
+}
+
+std::string naiveTrends::getNthPopular(unsigned int n){
+	std::sort(wordCountVector.begin(), wordCountVector.end(), compareFunc);
+	if (n <= numEntries()){
+		return wordCountVector[n].first;
+	}
+
+	//If they give bad input, return empty string.
+	return "";
+}
+
+unsigned int naiveTrends::numEntries(){
+	return wordCountVector.size();
+}
